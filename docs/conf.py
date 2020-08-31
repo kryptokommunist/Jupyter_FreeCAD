26a255c24967e2100b97f20cd0c04f01798d4a3b
Marcus Ding <marcusding@me.com>
2020-08-31 19:34:36 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ["FreeCADGui", "IPython", "pivy"]
+autodoc_mock_imports = ["FreeCADGui", "IPython", "pivy", "coin"]
 # -- Options for HTML output -------------------------------------------------
 
 # Add any paths that contain custom static files (such as style sheets) here,
