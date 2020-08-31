6bf482d2e2a76749d757f53a415159702772a243
kryptokommunist <marcusding@me.com>
2020-08-31 14:50:30 +0200

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -61,3 +61,6 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
