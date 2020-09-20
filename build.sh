378f68d17639f26d74a0d09481dd626e025229c1
Marcus Ding <marcusding@me.com>
2020-09-20 20:59:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ pip install jupyter
 
 pip install -r requirements.txt
 pip install runipy
+python -V
+python3 -V
+python3 -m pivy.coin
 runipy 'FreeCAD inside Jupyter Notebook - Examples.ipynb' --html report.html 
