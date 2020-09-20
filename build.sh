d5a572f81fb9ea2bef3cb955679352f10d531020
Marcus Ding <marcusding@me.com>
2020-09-20 20:55:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+sudo add-apt-repository -y ppa:freecad-maintainers/freecad-stable
+sudo apt-get -y update
+sudo apt-get -y install freecad-python3 python3-pivy
+pip install jupyter
+
 pip install -r requirements.txt
 pip install runipy
 runipy 'FreeCAD inside Jupyter Notebook - Examples.ipynb' --html report.html 
